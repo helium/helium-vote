e7896efe14f5928e98ec966d8cdf7a77cd6f908a
Daniel James <danielcolinjames@gmail.com>
2021-10-13 15:37:39 -0700

    Update config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@ module.exports = {
   serverRuntimeConfig: {
     votes: [
       {
-        id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZF1",
+        id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZFa",
         deadline: 1075000,
         link: "https://github.com/helium/HIP/blob/master/0039-hnt-redenomination.md",
         name: "HIP 39: HNT Redenomination",
-        description: "This proposal suggests a redenomination of the conversion rate between bones and HNT. Currently there are 100,000,000 (10^8) bones per HNT and under the proposed redenomination the conversion rate would be adjusted to 100,000 (10^5) bones per HNT.",
+        description:
+          "This proposal suggests a redenomination of the conversion rate between bones and HNT. Currently there are 100,000,000 (10^8) bones per HNT and under the proposed redenomination the conversion rate would be adjusted to 100,000 (10^5) bones per HNT.",
         outcomes: [
           {
             value: "For HNT Redenomination",
@@ -19,11 +20,12 @@ module.exports = {
         ],
       },
       {
-        id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZF2",
-        deadline: 10750000,
+        id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZFb",
+        deadline: 1075000,
         link: "https://github.com/helium/HIP/blob/master/0040-validator-denylist.md",
         name: "HIP 40: Validator Denylist",
-        description: "This plan proposes that validators would maintain a denylist file of Hotspot addresses which are selected from a basic floor function, selecting the hotspots where earnings are abnormal.",
+        description:
+          "This plan proposes that validators would maintain a denylist file of Hotspot addresses which are selected from a basic floor function, selecting the hotspots where earnings are abnormal.",
         outcomes: [
           {
             value: "For Validator Denylist",
