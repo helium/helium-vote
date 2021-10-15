35fd0f3025a8a21a9cd6454183031e97f7856a8a
Daniel James <danielcolinjames@gmail.com>
2021-10-15 11:00:27 -0700

    Add intro section to index page

--- pages/index.js
+++ pages/index.js
@@ -24,21 +24,36 @@ export default function Home({ height, votes }) {
         />
       </Head>
 
-      <ContentSection first>
+      <ContentSection flatBottom first>
         <div className="flex flex-col space-y-2">
-          <div>
-            <p className="text-lg pb-4 text-white">Active votes</p>
-            <div className="flex flex-col space-y-2">
-              {votes.map((v) => {
-                return (
-                  <Link href={`/${v.id}`}>
-                    <a className="w-full rounded-md border outline-none border-transparent focus:border-hv-blue-500 border-solid p-5 hover:bg-gray-300 bg-gray-600 transition-all duration-100 text-white text-2xl hover:text-gray-800">
-                      {v.name}
-                    </a>
-                  </Link>
-                );
-              })}
-            </div>
+          <div className="flex flex-col">
+            <h2 className="text-4xl font-sans text-white">Helium Vote</h2>
+
+            <p className="font-sans text-gray-300 max-w-2xl text-lg">
+              This is a paragraph text to talk about what Helium vote is.
+            </p>
+            <p className="font-sans text-gray-300 max-w-2xl text-lg">
+              This is a paragraph text to talk about what Helium vote is.
+            </p>
+            <p className="font-sans text-gray-300 max-w-2xl text-lg">
+              This is a paragraph text to talk about what Helium vote is.
+            </p>
+          </div>
+        </div>
+      </ContentSection>
+      <ContentSection flatTop>
+        <div>
+          <p className="text-lg pb-4 text-white">Active votes</p>
+          <div className="flex flex-col space-y-2">
+            {votes.map((v) => {
+              return (
+                <Link href={`/${v.id}`}>
+                  <a className="w-full rounded-md border outline-none border-transparent focus:border-hv-blue-500 border-solid p-5 hover:bg-gray-300 bg-gray-600 transition-all duration-100 text-white text-2xl hover:text-gray-800">
+                    {v.name}
+                  </a>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </ContentSection>
