a114135f30961bb4632c9b48cccc584f6662d304
Daniel James <danielcolinjames@gmail.com>
2021-10-15 17:21:03 -0700

    Use api.helium.io

--- data/client.js
+++ data/client.js
@@ -2,9 +2,10 @@ import Client, { Network } from "@helium/http";
 
 export const TAKE_MAX = 100000;
 
-const client = new Client(
-  new Network({ baseURL: "https://helium-api.stakejoy.com", version: 1 }),
-  { retry: 3 }
-);
+// const client = new Client(
+//   new Network({ baseURL: "https://helium-api.stakejoy.com", version: 1 }),
+//   { retry: 3 }
+// );
+const client = new Client();
 
 export default client;
