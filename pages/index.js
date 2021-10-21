7388af116afc0ec5debb4d3f1892d63eef069833
Daniel James <danielcolinjames@gmail.com>
2021-10-21 15:47:27 -0400

    Fix hover and focus states

--- pages/index.js
+++ pages/index.js
@@ -13,19 +13,18 @@ const VoteCard = ({ vote, height }) => {
   const blocksRemaining = deadline - height;
 
   return (
-    <Link href={`/${id}`}>
-      <a className="flex-shrink-0 min-w-96 w-full sm:w-96 sm:p-5 border outline-none border-opacity-0 sm:hover:border-opacity-100 border-hv-purple-500 border-dashed duration-100 transition-all sm:focus:border-opacity-50">
-        <div className="h-full w-full flex flex-col transition-all duration-150">
-          <span className="pt-5 px-5 rounded-t-3xl bg-hv-gray-450 relative">
+    <div className="flex-shrink-0 min-w-96 w-full sm:w-96 sm:p-5">
+      <Link href={`/${id}`}>
+        <a className="group h-full w-full flex flex-col transition-all duration-150 outline-none border border-solid border-opacity-0 focus:border-opacity-50 border-hv-green-500 rounded-3xl">
+          <span className="pt-5 px-5 rounded-t-3xl bg-hv-gray-450 group-hover:bg-hv-gray-400 transition-all duration-150">
             <p className="text-white text-xl tracking-tight leading-tight pb-2">
               {name}
             </p>
-            <p className="text-gray-500 h-14 overflow-hidden text-sm leading-tight">
-              {description}
+            <p className="text-hv-gray-300 h-14 overflow-hidden text-sm leading-tight">
+              {description.substring(0, 80)}...
             </p>
-            <div className="absolute h-10 bottom-0 left-0 right-0 bg-gradient-to-t from-hv-gray-450 to-transparent" />
           </span>
-          <div className="rounded-b-3xl bg-hv-gray-500 p-5 flex flex-col">
+          <div className="rounded-b-3xl bg-hv-gray-500 group-hover:bg-hv-gray-550 transition-all duration-150 p-5 flex flex-col">
             <span className="text-sm text-hv-gray-200">
               Est. Time Remaining
             </span>
@@ -33,9 +32,9 @@ const VoteCard = ({ vote, height }) => {
               <CountdownTimer blocksRemaining={blocksRemaining} key={id} />
             </span>
           </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </div>
   );
 };
 
@@ -81,18 +80,20 @@ export default function Home({ height, activeVotes, completedVotes }) {
                     href="https://github.com/helium/helium-vote"
                     rel="noopener noreferrer"
                     target="_blank"
-                    className="opacity-40 flex flex-row items-center justify-start hover:opacity-75 transition-all duration-150"
+                    className="inline-block outline-none border-hv-green-500 border-opacity-0 border border-solid rounded-md focus:border-opacity-100 opacity-40 hover:opacity-75 transition-all duration-150"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="text-white w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                    </svg>
-                    <span className="text-md text-gray-500 font-semibold pl-2">
-                      @helium/helium-vote
+                    <span className="flex flex-row items-center justify-start">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="text-white w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                      </svg>
+                      <span className="text-md text-gray-500 font-semibold pl-2">
+                        @helium/helium-vote
+                      </span>
                     </span>
                   </a>
                   <h2 className="text-4xl sm:text-8xl font-sans font-semibold text-white tracking-tighter">
