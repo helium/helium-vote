eed5452cc3df70b19cc0e820b481a2feb3dd40a7
Daniel James <danielcolinjames@gmail.com>
2021-10-22 16:29:34 -0400

    Fix config

--- next.config.js
+++ next.config.js
@@ -62,24 +62,24 @@ module.exports = {
           },
         ],
       },
-      {
-        id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZFb",
-        deadline: 1075000,
-        link: "https://github.com/helium/HIP/blob/master/0040-validator-denylist.md",
-        name: "HIP 40: Validator Denylist",
-        description:
-          "This plan proposes that validators would maintain a denylist file of Hotspot addresses which are selected from a basic floor function, selecting the hotspots where earnings are abnormal.",
-        outcomes: [
-          {
-            value: "For Validator Denylist",
-            address: "13DB1TNxeCiVATE8tfC38NisYXJebx59K2SCXY6tuWv2McYW1rw",
-          },
-          {
-            value: "Against Validator Denylist",
-            address: "14QKWs6GhqJkrXhwj2r3aXJFQVN5TbhHfDMnkGGAtxTg5apx32M",
-          },
-        ],
-      },
+      // {
+      //   id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZFb",
+      //   deadline: 1075000,
+      //   link: "https://github.com/helium/HIP/blob/master/0040-validator-denylist.md",
+      //   name: "HIP 40: Validator Denylist",
+      //   description:
+      //     "This plan proposes that validators would maintain a denylist file of Hotspot addresses which are selected from a basic floor function, selecting the hotspots where earnings are abnormal.",
+      //   outcomes: [
+      //     {
+      //       value: "For Validator Denylist",
+      //       address: "13DB1TNxeCiVATE8tfC38NisYXJebx59K2SCXY6tuWv2McYW1rw",
+      //     },
+      //     {
+      //       value: "Against Validator Denylist",
+      //       address: "14QKWs6GhqJkrXhwj2r3aXJFQVN5TbhHfDMnkGGAtxTg5apx32M",
+      //     },
+      //   ],
+      // },
     ],
   },
 };
