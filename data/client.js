1986def4a947ddea82487d76ab9d832c3260399c
Daniel James <danielcolinjames@gmail.com>
2021-11-01 11:42:13 -0700

    Change API temporarily for ?max_block fix

--- data/client.js
+++ data/client.js
@@ -2,9 +2,9 @@ import Client, { Network } from "@helium/http";
 
 export const TAKE_MAX = 100000;
 
-const client = new Client(Network.stakejoy);
+// const client = new Client(Network.stakejoy);
 //
-// const client = new Client(Network.staging);
+const client = new Client(Network.staging);
 //
 // const client = new Client();
 
