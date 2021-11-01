e09e55994b8b0c4f0bba1a4327b402653205c8da
Daniel James <danielcolinjames@gmail.com>
2021-11-01 15:57:19 -0700

    Update config

--- next.config.js
+++ next.config.js
@@ -71,32 +71,31 @@ module.exports = {
           {
             value: "Vanilla",
             address: "13yWhaorHn8Es6jujCw9HCFAjDyecCv5HMwzoa4gp26awSw7z3b",
-            color: "#EEEEEE",
           },
         ],
       },
-      {
-        id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZFb",
-        deadline: 1095000,
-        link: "https://github.com/helium/HIP/blob/master/0040-validator-denylist.md",
-        name: "HIP 40: Validator Denylist",
-        description:
-          "This plan proposes that validators would maintain a denylist file of Hotspot addresses which are selected from a basic floor function, selecting the hotspots where earnings are abnormal.",
-        tags: {
-          primary: "HIP 40",
-          secondary: "Validators",
-        },
-        outcomes: [
-          {
-            value: "For Validator Denylist",
-            address: "13DB1TNxeCiVATE8tfC38NisYXJebx59K2SCXY6tuWv2McYW1rw",
-          },
-          {
-            value: "Against Validator Denylist",
-            address: "14QKWs6GhqJkrXhwj2r3aXJFQVN5TbhHfDMnkGGAtxTg5apx32M",
-          },
-        ],
-      },
+      // {
+      //   id: "13YxdDMkDsjnWAk1MSBKADveZm5RnMTLEqq1yTvzHKBZnvjsZFb",
+      //   deadline: 1095000,
+      //   link: "https://github.com/helium/HIP/blob/master/0040-validator-denylist.md",
+      //   name: "HIP 40: Validator Denylist",
+      //   description:
+      //     "This plan proposes that validators would maintain a denylist file of Hotspot addresses which are selected from a basic floor function, selecting the hotspots where earnings are abnormal.",
+      //   tags: {
+      //     primary: "HIP 40",
+      //     secondary: "Validators",
+      //   },
+      //   outcomes: [
+      //     {
+      //       value: "For Validator Denylist",
+      //       address: "13DB1TNxeCiVATE8tfC38NisYXJebx59K2SCXY6tuWv2McYW1rw",
+      //     },
+      //     {
+      //       value: "Against Validator Denylist",
+      //       address: "14QKWs6GhqJkrXhwj2r3aXJFQVN5TbhHfDMnkGGAtxTg5apx32M",
+      //     },
+      //   ],
+      // },
     ],
   },
   webpack: (config, { isServer }) => {
