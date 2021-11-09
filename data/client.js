594aca7c4147393df1da8d42997c9f9df3e226e4
Abhay Kumar <abhay@helium.com>
2021-11-09 08:50:15 -0800

    Switch to prod

--- data/client.js
+++ data/client.js
@@ -4,8 +4,8 @@ export const TAKE_MAX = 100000;
 
 // const client = new Client(Network.stakejoy);
 //
-const client = new Client(Network.staging);
+// const client = new Client(Network.staging);
 //
-// const client = new Client();
+const client = new Client();
 
 export default client;
