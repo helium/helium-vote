cb802009af30a83fecac7d10ab32da754843a7a6
Daniel James <danielcolinjames@gmail.com>
2021-11-15 09:57:06 -0800

    Vote Options header fix on mobile

--- components/VoteOptionsSection.js
+++ components/VoteOptionsSection.js
@@ -12,7 +12,7 @@ const VoteOptionsSection = ({ outcomes }) => {
     <div className="w-full bg-hv-gray-750 py-5 sm:py-10 mt-10 sm:mt-20">
       <div className="flex flex-col space-y-2 max-w-5xl mx-auto px-0 sm:px-10">
         <div>
-          <p className="text-xl mb-3 tracking-tight sm:text-3xl font-semibold text-white font-sans pb-4">
+          <p className="text-xl ml-4 sm:ml-0 mb-3 tracking-tight sm:text-3xl font-semibold text-white font-sans pb-4">
             Vote Options
           </p>
           <div className="w-full">
