3c8a421d2b170988c84ee3dec70299736135264d
Daniel James <danielcolinjames@gmail.com>
2022-03-29 10:28:14 -0700

    Update clientside endpoint

--- data/client.js
+++ data/client.js
@@ -6,26 +6,12 @@ const client = new Client(
   new Network({
     // the only request this client will make is to grab the current block height
     // the results calculation client is defined in /jobs/client.js
-    baseURL: "https://helium-api.stakejoy.com",
+    baseURL: "https://api.helium.io",
     version: 1,
   }),
   {
     userAgent: "helium-vote-site",
   }
 );
-
-// const client = new Client(
-//   new Network({
-//     baseURL: "https://tileserver.skittles.stakejoy.com",
-//     version: 1,
-//   }),
-//   {
-//     userAgent: "helium-vote",
-//   }
-// );
-//
-// const client = new Client(Network.staging);
-//
-// const client = new Client();
 
 export default client;
