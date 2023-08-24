5c39bdf91a732a5a75179326991746b2af010c71
Chewing Glass <chewinglass123@gmail.com>
2023-08-24 11:05:46 -0500

    Fix header link

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ export function Header({
   return (
     <nav className="border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center">
+        <a href={`/?network=${network}`} className="flex items-center">
           <img src="/images/logo.svg" className="h-8 mr-3" alt="Helium Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Helium
