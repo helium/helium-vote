38b2a0ea07d07446c76d2b15fe32c154156e1ba7
Chewing Glass <chewinglass123@gmail.com>
2023-10-11 16:28:54 -0400

    Add script for test hip

---
+++ bin/hips/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./bin/helium-vote.ts create-proposal \
+  -n "Test Vote: Top IOT Sensor Solutions" \
+  --proposalUri "https://gist.githubusercontent.com/hiptron/9a017901a42c91bdb7e38a8ff9544600/raw/5dfe487568d0817d3e1e992c2c498d670639ebe2/Test%2520Vote:%2520Top%2520IOT%2520Sensor%2520Solutions.md" \
+  -u "https://solana-rpc.web.helium.io?session-key=Pluto" \
+  --orgName Helium \
+  --choices "Tracking devices or people" \
+  --choices "Location Temp/Humidity" \
+  --choices "Location CO2/Air Quality" \
+  --choices "Movement/Presence detection" \
+  --choices "Soil quality/environment" \
+  --maxChoicesPerVoter 3
