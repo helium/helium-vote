540b4b2f4b875549466789fc375f4afd968838bc
Chewing Glass <chewinglass123@gmail.com>
2023-10-12 17:35:19 -0400

    Don't use anchor provider

--- hooks/useVotes.ts
+++ hooks/useVotes.ts
@@ -1,15 +1,18 @@
-import { Program } from "@coral-xyz/anchor";
-import { useAnchorProvider } from "@helium/helium-react-hooks";
+import { AnchorProvider, Program } from "@coral-xyz/anchor";
 import { VoterStakeRegistry } from "@helium/idls/lib/types/voter_stake_registry";
 import { init } from "@helium/voter-stake-registry-sdk";
+import { useConnection } from "@solana/wallet-adapter-react";
 import { PublicKey } from "@solana/web3.js";
 import { useAsync } from "react-async-hook";
 
 export function useVotes(proposal: PublicKey) {
-  const provider = useAnchorProvider();
-  const { result: sdk, loading: loadingSdk } = useAsync(
-    async (provider) => init(provider),
-    [provider]
+  const { connection: conn } = useConnection();
+  const {
+    result: sdk,
+    loading: loadingSdk,
+  } = useAsync(
+    async (conn) => init(new AnchorProvider(conn, {} as any, {})),
+    [conn]
   );
   // @ts-ignore
   const { result: markers, loading } = useAsync(
