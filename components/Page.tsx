b1992bc434920eb0d668b54f25451be2d6e3b756
Bryan <bryanzettler@gmail.com>
2023-12-07 10:32:45 -0600

    Bump deps (#95)

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,18 +1,23 @@
 import { HeliumVsrStateProvider } from "@helium/voter-stake-registry-hooks";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { useNetwork } from "../hooks/useNetwork";
 import { Header } from "./Header";
+import { Wallet } from "@coral-xyz/anchor";
+import { useAnchorProvider } from "@helium/helium-react-hooks";
+import { useNetwork } from "../hooks/useNetwork";
 
 const Page: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-  const { publicKey } = useWallet();
-  const { network, setNetwork, mint, setMint } = useNetwork();
+  const anchorProvider = useAnchorProvider();
+  const { mint, setMint } = useNetwork();
 
   return (
     <>
       <main className="w-full min-h-screen">
         <Header votingMint={mint} setVotingMint={setMint} />
         <div className="min-h-screen w-full pb-20 py-10">
-          <HeliumVsrStateProvider wallet={publicKey} mint={mint}>
+          <HeliumVsrStateProvider
+            connection={anchorProvider?.connection}
+            wallet={anchorProvider?.wallet as Wallet}
+            mint={mint}
+          >
             {children}
           </HeliumVsrStateProvider>
         </div>
