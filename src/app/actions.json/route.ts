1b5b01446e1cfea1ff1297d691b320bed2bfc9d7
Abhay Kumar <75+abhay@users.noreply.github.com>
2024-06-23 09:39:53 -0700

    Add Allow-Origin from anywhere (#155)

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -2,16 +2,17 @@ import { NextResponse } from "next/server";
 
 // To handle a GET request to /api
 export async function GET() {
-  // Do whatever you want
-  return NextResponse.json(
-    {
-      rules: [
+    let response = NextResponse.json(
         {
-          pathPattern: "/*/proposals/**",
-          apiPath: "https://actions.dialect.to/api/helium/*/proposals/**",
+            rules: [
+                {
+                    pathPattern: "/*/proposals/**",
+                    apiPath: "https://actions.dialect.to/api/helium/*/proposals/**",
+                },
+            ],
         },
-      ],
-    },
-    { status: 200 }
-  );
+        { status: 200 }
+    );
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    return response;
 }
