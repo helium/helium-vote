a22a985e475c2c3bc6209772449c86e403de6c10
Noah Prince <83885631+ChewingGlass@users.noreply.github.com>
2024-07-17 09:06:40 -0700

    Use helium blink route (#159)

--- src/app/actions.json/route.ts
+++ src/app/actions.json/route.ts
@@ -3,15 +3,15 @@ import { NextResponse } from "next/server";
 // To handle a GET request to /api
 export async function GET() {
     let response = NextResponse.json(
-        {
-            rules: [
-                {
-                    pathPattern: "/*/proposals/**",
-                    apiPath: "https://actions.dialect.to/api/helium/*/proposals/**",
-                },
-            ],
-        },
-        { status: 200 }
+      {
+        rules: [
+          {
+            pathPattern: "/*/proposals/**",
+            apiPath: "https://helium.dial.to/*/proposals/**",
+          },
+        ],
+      },
+      { status: 200 }
     );
     response.headers.set("Access-Control-Allow-Origin", "*");
     return response;
