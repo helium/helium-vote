becede61bade0987cface84988bce1bf9c1417f3
Abhay Kumar <75+abhay@users.noreply.github.com>
2025-02-28 09:24:18 -0800

    Follow on updates for HIP 138 and 141 (#199)
    * bump deps (#200)
    
    * Update copy
    
    * Allow reclaiming subnetwork token positions, again
    
    * sig
    
    ---------
    
    Co-authored-by: Bryan <bryanzettler@gmail.com>

--- src/components/CreatePositionModal.tsx
+++ src/components/CreatePositionModal.tsx
@@ -207,11 +207,8 @@ export const CreatePositionModal: FC<React.PropsWithChildren<{}>> = ({
                   </span>
                   <ul className="flex flex-col px-6 list-disc font-light">
                     <li>You believe in that subnetwork</li>
-                    <li>
-                      You wish to receive either MOBILE or IOT tokens as rewards
-                      (which can be claimed on a daily basis)
-                    </li>
-                    <li>You help increase the DAO Utility Score</li>
+                    <li>You want to help increase the subnetworks Protocol Score</li>
+                    <li>You are willing to be an active participant in governance in order to receive HNT rewards (which can be claimed on a daily basis)</li>
                   </ul>
                 </div>
                 <div>
